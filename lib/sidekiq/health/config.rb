cc926f10aeeeb060e45ca2bf6a5c27f6a859f511
Gustavo Pchek <ghpk88@gmail.com>
2024-09-02 20:13:37 -0300

    fix warning at config

--- lib/sidekiq/health/config.rb
+++ lib/sidekiq/health/config.rb
@@ -8,11 +8,11 @@ module Sidekiq
         maximum_healthy_dead_set_last_hour_size: 10
       }.freeze
 
-      def maximum_healthy_queue_size
+      def resolved_maximum_healthy_queue_size
         self[:maximum_healthy_queue_size] || DEFAULTS[:maximum_healthy_queue_size]
       end
 
-      def maximum_healthy_dead_set_last_hour_size
+      def resolved_maximum_healthy_dead_set_last_hour_size
         self[:maximum_healthy_dead_set_last_hour_size] || DEFAULTS[:maximum_healthy_dead_set_last_hour_size]
       end
     end
